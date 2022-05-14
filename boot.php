bec68e2b4dbe02caa161590f596413533821f7d1
Alexandr Toropov <toropovsite@yandex.ru>
2022-05-14 17:48:18 +0300

    Добавил проверку на наличие файла config.php

--- boot.php
+++ boot.php
@@ -9,7 +9,12 @@ function pdo(): PDO
     static $pdo;
 
     if (!$pdo) {
-        $config = include __DIR__.'/config.php';
+        if (file_exists(__DIR__ . '/config.php')) {
+            $config = include __DIR__.'/config.php';
+        } else {
+            $msg = 'Создайте и настройте config.php на основе config.sample.php';
+            trigger_error($msg, E_USER_ERROR);
+        }
         // Подключение к БД
         $dsn = 'mysql:dbname='.$config['db_name'].';host='.$config['db_host'];
         $pdo = new PDO($dsn, $config['db_user'], $config['db_pass']);
